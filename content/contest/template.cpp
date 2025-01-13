8728259b9da9c84895acac9ad9cd95ff9aa2ec0b
johankogoodvibes <johnygott@gmail.com>
2025-01-13 18:04:25 +0100

    minor starting updates

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,44 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
 typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+typedef pair<ll, ll> pll;
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+#define For(i, a, n) for(ll i = a;i<(ll)n;i++)
+#define vec vector
+#define all(x) begin(x), end(x)
+#define endl "\n"
+
+template<class A, class B>
+pair<A, B> operator+(const pair<A, B>& a, const pair<A, B>& b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template<class A, class B>
+ostream& operator<<(ostream& os, const pair<A, B>&a){
+   return os<<"("<<a.first<<", "<<a.second<<")";
+    
+}
+template<class A, class B, class C>
+basic_ostream<A, B>& operator<<(basic_ostream<A, B>& os, const C c){
+    for(auto it = begin(c);it!=end(c);++it){
+        os<<(it==begin(c)?"":" ")<<*it;
+    }
+    return os;
+}
+template<typename... Args>
+void dbg(Args&&... args){
+    ((cerr<<args<<" |"),...);
+    cerr<<endl;
+}
+void solve(){
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    ll t = 1;//cin>>t;
+    while(t--){
+        solve();
+    }
 }
--- content/graph/DFSMatching.h
+++ content/graph/DFSMatching.h
@@ -1,38 +1,40 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-28
+ * Author: Jan Gottweis
+ * Date: 2025-1-3
  * License: CC0
  * Source:
- * Description: Simple bipartite matching algorithm. Graph $g$ should be a list
- * of neighbors of the left partition, and $btoa$ should be a vector full of
- * -1's of the same size as the right partition. Returns the size of
+ * Description: UPDATED g is of size $|A|$, contains neighbors in B (numbers in $0,...,|B|$) 
+ * $btoa$ should be a vector full of -1's of size $|B|$. Returns the size of
  * the matching. $btoa[i]$ will be the match for vertex $i$ on the right side,
  * or $-1$ if it's not matched.
  * Time: O(VE)
- * Usage: vi btoa(m, -1); dfsMatching(g, btoa);
+ * Usage: vec<ll> btoa(m, -1); dfsMatching(g, btoa);
  * Status: works
  */
 #pragma once
 
-bool find(int j, vector<vi>& g, vi& btoa, vi& vis) {
-	if (btoa[j] == -1) return 1;
-	vis[j] = 1; int di = btoa[j];
-	for (int e : g[di])
-		if (!vis[e] && find(e, g, btoa, vis)) {
-			btoa[e] = di;
-			return 1;
-		}
-	return 0;
+bool find(ll j, vec<vec<ll>>&g, vec<ll>&btoa, vec<ll>&vis){
+    if(btoa[j] == -1)return 1;
+    vis[j] = 1; ll di = btoa[j];
+    for(ll e : g[di]){
+        if(!vis[e] && find(e, g, btoa, vis)){
+            btoa[e] = di;
+            return 1;
+        }
+    }
+    return 0;
 }
-int dfsMatching(vector<vi>& g, vi& btoa) {
-	vi vis;
-	rep(i,0,sz(g)) {
-		vis.assign(sz(btoa), 0);
-		for (int j : g[i])
-			if (find(j, g, btoa, vis)) {
-				btoa[j] = i;
-				break;
-			}
-	}
-	return sz(btoa) - (int)count(all(btoa), -1);
+
+ll dfsMatching(vec<vec<ll>>&g, vec<ll>&btoa){
+    vec<ll> vis;
+    For(i, 0, sz(g)){
+        vis.assign(sz(btoa), 0);
+        for(ll j : g[i]){
+            if(find(j, g, btoa, vis)){
+                btoa[j] = i;
+                break;
+            }
+        }
+    }
+    return sz(btoa)- (ll)count(all(btoa), -1);
 }
